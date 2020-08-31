151a8b67d402dcd0de4f965da0f54b730bb818f3
Charlesccharris <70536520+Charlesccharris@users.noreply.github.com>
2020-08-31 13:19:04 -0500

    Update HomeWork2_21.java
    Built the basic framework for the investment calculations

--- HomeWork2_21.java
+++ HomeWork2_21.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 
 class HomeWork2_21{
-	public static void main(String[] args){
 	/*2.21 (Financial application: calculate future investment value) 
 	Write a program that reads in investment amount, annual interest rate, and number of years, 
 	and displays the future investment value using the following formula: 
@@ -11,20 +10,21 @@ class HomeWork2_21{
 	For example, if you enter amount 1000, annual interest rate 3.25%, and number of years 1, the future investment value is 1032.98. 
 	Here is a sample run: Enter investment amount: 1000.56 Enter annual interest rate in percentage: 4.25 Enter number of years: 1 Accumulated value is $1043.92*/
 
-	Scanner input = new Scanner(System.in);
-	DecimalFormat df = new DecimalFormat("######.##");
 
 	//To get the input for the investment amount
-	System.out.print("Initial Investment Amount : ");
-	double investmentAmount = input.nextDouble();
+  	double investmentAmount = 1000;
+	System.out.print("Initial Investment Amount : " + investmentAmount);
+	
 
 	//To get the input for the annual interest rate
-	System.out.print("Annual Interest Rate (in percentage): ");
-	double annualInterestRate = input.nextDouble();
+  	double annualInterestRate = 3.25;
+	System.out.println("Annual Interest Rate (in percentage): " + annualInterestRate);
+	
 
-	//To get how long the investment is for
-	System.out.print("Number of Years : ");
-	double numberOfYears = input.nextDouble();
+	//To get how long the investment is for	
+  	double numberOfYears = 1;
+	System.out.println("Number of Years : " + numberOfYears);
+
 
 	double monthlyInterestRate = annualInterestRate/12/100;
 
@@ -38,7 +38,7 @@ class HomeWork2_21{
 
 	//To output the results of the calculations
 	System.out.println("With an initial investment of $" + investmentAmount + " and an annual interest rate of "
-	+ annualInterestRate + "% for " + numberOfYears + " year(s), the future investment value is $" + (df.format(futureInvestmentValue)));
+	+ annualInterestRate + "% for " + numberOfYears + " year(s), the future investment value is $" + futureInvestmentValue);
 
 	}
 }
