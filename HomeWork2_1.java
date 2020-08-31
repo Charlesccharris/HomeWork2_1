278017173f3f0fe7dec3959a92132d6b3aab1f3b
Charlesccharris <70536520+Charlesccharris@users.noreply.github.com>
2020-08-31 13:05:36 -0500

    Update HomeWork2_1.java
    Added input for Celsius

--- HomeWork2_1.java
+++ HomeWork2_1.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 class HomeWork2_1{
 	public static void main(String[] args){
 		/*2.1 (Convert Celsius to Fahrenheit) 
@@ -7,8 +10,13 @@ class HomeWork2_1{
 		Hint: In Java, 9 / 5 is 1, but 9.0 / 5 is 1.8. 
 		Here is a sample run:Enter a degree in Celsius: 43. 43 Celsius is 109.4 Fahrenheit.*/
 
-		double celsius = 43;
-				
+
+		Scanner input = new Scanner(System.in);
+
+		//Celsius input
+		System.out.print("Enter a number for Celsius: ");
+		double celsius = input.nextDouble();
+
 		//Fahrenheit output
 		double fahrenheit = (9.0/5.0) * celsius + 32;
 		System.out.println(celsius + " degrees Celsius is " + fahrenheit + " degrees Fahrenheit");
